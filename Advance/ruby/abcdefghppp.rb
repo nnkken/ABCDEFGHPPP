70d5d8f9d41a4d78fb33c97625e032b661466686
nnkken <imwmbm@gmail.com>
2016-03-31 12:11:36 +0800

    Enhanced Advance-Ruby performance.

--- Advance/ruby/abcdefghppp.rb
+++ Advance/ruby/abcdefghppp.rb
@@ -15,7 +15,7 @@ else
 end
 
 class Solution
-    RANGE = (0...BASE).to_a
+    RANGE = (0...BASE).to_a - [1]
 
     PRECOMPUTE_SUM = (0..(2 * BASE)).map { |sum|
         [
@@ -29,6 +29,31 @@ class Solution
             }
         ]
     }
+    .to_h
+
+    POS_SUM = (0...WIDTH).map do |k1|
+        k1 = k1 + 2 * WIDTH
+        [
+            k1,
+            (0...WIDTH).map do |k2|
+                k2 = k2 + 3 * WIDTH
+                [
+                    k2,
+                    PRECOMPUTE_SUM.map do |sum, pairs|
+                        if k1 == 2 * WIDTH
+                            pairs = pairs.select { |x, y| x != 0 }
+                        end
+                        if k2 == 3 * WIDTH
+                            pairs = pairs.select { |x, y| y != 0 }
+                        end
+                        [sum, pairs]
+                    end
+                    .to_h
+                ]
+            end
+            .to_h
+        ]
+    end
     .to_h
 
     PRECOMPUTE_DIFF = (-BASE..BASE).map { |diff|
@@ -45,10 +70,33 @@ class Solution
     }
     .to_h
 
-    def initialize(sol = nil)
+    POS_DIFF = (0...WIDTH).map do |k1|
+        [
+            k1,
+            (0...WIDTH).map do |k2|
+                k2 = k2 + WIDTH
+                [
+                    k2,
+                    PRECOMPUTE_DIFF.map do |diff, pairs|
+                        if k1 == 0
+                            pairs = pairs.select { |x, y| x != 0 }
+                        end
+                        if k2 == WIDTH
+                            pairs = pairs.select { |x, y| y != 0 }
+                        end
+                        [diff, pairs]
+                    end
+                    .to_h
+                ]
+            end
+            .to_h
+        ]
+    end
+    .to_h
+
+    def initialize(sol = nil, index = nil)
         @sol = (sol.nil? ? Array.new(DIGITS) : sol)
-        @index = []
-        @sol.compact.each { |v| @index[v] = true }
+        @index = (index.nil? ? 0 : index)
     end
 
     def [](key)
@@ -57,39 +105,37 @@ class Solution
 
     def set(kv)
         newSol = @sol.clone
+        newIndex = @index
         kv.each do |key, value|
             newSol[key] = value
+            newIndex |= (1 << value)
         end
-        Solution.new(newSol)
+        Solution.new(newSol, newIndex)
+    end
+
+    def setPair(k1, v1, k2, v2)
+        newSol = @sol.clone
+        newIndex = @index
+        newSol[k1] = v1
+        newSol[k2] = v2
+        newIndex |= (1 << v1) | (1 << v2)
+        Solution.new(newSol, newIndex)
     end
 
     def pairs(k1, k2, from)
         from
-        .select { |x, y| !@index[x] && !@index[y] }
-        .map { |x, y| set([[k1, x], [k2, y]]) }
-        .select { |sol| sol.accept? }
+        .select { |x, y| (@index & (1 << x)) == 0 && (@index & (1 << y)) == 0 }
+        .map { |x, y| setPair(k1, x, k2, y) }
     end
 
     def pairsWithSum(k1, k2, sum)
         pairs(k1, k2, PRECOMPUTE_SUM[sum])
+        pairs(k1, k2, POS_SUM[k1][k2][sum])
     end
 
     def pairsWithDiff(k1, k2, diff)
-        pairs(k1, k2, PRECOMPUTE_DIFF[diff])
-    end
-
-    W1 = WIDTH
-    W2 = 2 * WIDTH
-    W3 = 3 * WIDTH
-    W4 = 4 * WIDTH
-    def accept?
-        !(
-            @sol[0] == 0 ||
-            @sol[W1] == 0 ||
-            @sol[W2] == 0 ||
-            @sol[W3] == 0 ||
-            @sol[W4] == 0
-        )
+        # pairs(k1, k2, PRECOMPUTE_DIFF[diff])
+        pairs(k1, k2, POS_DIFF[k1][k2][diff])
     end
 
     def to_s
@@ -112,12 +158,12 @@ end
 
 baseSolution = Solution.new.set([[DIGITS - 1, 1]])
 solutions = baseSolution
-.pairsWithSum(DIGITS - 2, DIGITS - WIDTH - 2, BASE + 1)
+.pairsWithSum(DIGITS - WIDTH - 2, DIGITS - 2, BASE + 1)
 .lazy
 
 (3..(WIDTH + 1)).each do |w|
     solutions = solutions.flat_map { |sol|
-        sol.pairsWithSum(DIGITS - w, DIGITS - WIDTH - w, BASE)
+        sol.pairsWithSum(DIGITS - WIDTH - w, DIGITS - w, BASE)
     }
 end
 
